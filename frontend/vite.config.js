95d26e528732f9f21877576d4af107a6968160ff
anchanung <1stevering@naver.com>
2026-07-14 17:36:05 +0900

    fix: 에러 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+
   server: {
     proxy: {
       "/api": "http://localhost:8000",
